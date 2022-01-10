cc61aaee30802c26bb5bc64defb705296353c8a1
MirjanaR <57662399+mirjana123@users.noreply.github.com>
2022-01-10 20:00:23 +0100

    Add files via upload

---
+++ pripremaZaDrugiKlk/Common/Korisnik.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{   
+    [Serializable]
+    public class Korisnik
+    {
+        public override bool Equals(object obj)
+        {
+            var v = obj as Korisnik;
+            return (v != null && v.Email == this.Email && v.Sifra == this.Sifra);
+        }
+        public override string ToString()
+        {
+            return Email;
+        }
+
+
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Email { get; set; }
+        public string Sifra { get; set; }
+
+    }
+}
